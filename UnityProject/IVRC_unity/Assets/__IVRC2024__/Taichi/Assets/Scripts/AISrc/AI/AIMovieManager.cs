ddf1430c0a1c5a3ded2e209a7147e94c49089f15
yayoi-exe <shiratai33@docomo.ne.jp>
2024-09-11 10:08:06 +0900

    update

--- UnityProject/IVRC_unity/Assets/__IVRC2024__/Taichi/Assets/Scripts/AISrc/AI/AIMovieManager.cs
+++ UnityProject/IVRC_unity/Assets/__IVRC2024__/Taichi/Assets/Scripts/AISrc/AI/AIMovieManager.cs
@@ -6,14 +6,53 @@ using UnityEngine.SceneManagement;
 public class AIMovieManager : MonoBehaviour
 {
     public string sceneName;
-    
+
+    // 2つのAudioSourceを用意
+    public AudioSource newBGMAudioSource;
+    public AudioSource audioSource;
+    public AudioSource lastBGMAudioSource;
+
+    // 2つの音声クリップ
+    public AudioClip newBGMClip;
+    public AudioClip audioClip;
+    public float delay = 2.0f;
+
     public void StartMovie()
     {
         Debug.Log("StartMovie");
+
+        StopAudioClips();
+
+        // 指定した待機時間後に音声クリップを再生
+        StartCoroutine(PlayAudioClipsWithDelay(newBGMAudioSource, newBGMClip, 0.0f));
+        StartCoroutine(PlayAudioClipsWithDelay(audioSource, audioClip, delay));
+
+        ChangeScene();
     }
 
     void ChangeScene()
     {
         SceneManager.LoadScene(this.sceneName);
+    }
+
+    // 待機後に2つの音声を同時に再生するメソッド
+    IEnumerator PlayAudioClipsWithDelay(AudioSource audioSource, AudioClip clip, float delay)
+    {
+        // delayの時間待機
+        yield return new WaitForSeconds(delay);
+        if (audioSource != null && clip != null)
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+    }
+
+    // 2つのAudioSourceを停止するメソッド
+    public void StopAudioClips()
+    {
+        if (lastBGMAudioSource != null && lastBGMAudioSource.isPlaying)
+        {
+            lastBGMAudioSource.Stop();
+        }
     }
 }
